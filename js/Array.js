056e53c265c7038cbb574a39f507aaf18eebdbe0
Sachin <110269869+sachinseb@users.noreply.github.com>
2025-06-01 13:25:00 +0530

    ggghg

--- js/Array.js
+++ js/Array.js
@@ -3,6 +3,7 @@
 //     color = black
 //     specs = intel
 
+
 /*
 const Laptop = {
     id : 4567,
@@ -16,9 +17,47 @@ console.log(Laptop.specs)
 */
 
 
-let arr = ["red", "green", "blue", 'sachin', 'Anup', 'Atul', 'Tushar'];
+// let arr = ["red", "green", "blue", 'sachin', 'Anup', 'Atul', 'Tushar'];
 
 
-console.log(arr.splice(1, 2, "x", "y"))
+// console.log(arr.splice(1, 2, "x", "y"))
 
-console.log(arr)
+// console.log(arr)
+
+
+
+// function myFunc() {
+//     let arr = [1, 2, 3];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         console.log(arr[i]); // 2
+//     }
+// }
+
+// i = 2
+// arr[2] = 3
+// i = i - 1, 2 - 1, 1
+// arr[1] = 2
+// arr[0] = 1
+
+// myFunc()
+
+num = [1,2]
+console.log(num.push(3))
+console.log(num)
+
+
+function push()
+{
+    
+}
+// let arr = [1, 2, 3, 4, 5, 6]; // 3 2
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//         console.log(arr[i])
+//     }
+
+// }
+
+// 0  % 2 == 0
+// 1  % 2 == 0
+// 2  % 2 == 0

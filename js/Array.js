d5017b9b2f596a5efd33c1aad2befb07adeb10e5
Sachin <110269869+sachinseb@users.noreply.github.com>
2025-06-01 12:50:23 +0530

    jgjg

---
+++ js/Array.js
@@ -0,0 +1,24 @@
+// laptop  
+//     id = 4567
+//     color = black
+//     specs = intel
+
+/*
+const Laptop = {
+    id : 4567,
+    color : "black",
+    specs : "intel"
+}
+
+console.log(Laptop.id)
+console.log(Laptop.color)
+console.log(Laptop.specs)
+*/
+
+
+let arr = ["red", "green", "blue", 'sachin', 'Anup', 'Atul', 'Tushar'];
+
+
+console.log(arr.splice(1, 2, "x", "y"))
+
+console.log(arr)
